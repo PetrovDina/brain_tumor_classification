fab22c6c0fd3023d44e5e2a4b6c5bc303523f032
Dina Petrov <dina.petrov@gmail.com>
2021-07-01 16:14:10 +0200

    Model

--- brain_tumor_classification.py
+++ brain_tumor_classification.py
@@ -1,38 +1,31 @@
 
-# importi sa vezbi
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D # global dodat
-from keras import backend as K, Model
 
-import numpy as np
 import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras import backend as K, Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+import matplotlib.pyplot as plt
+import numpy as np
 import cv2
-#from tqdm import tqdm #ovo nam vrv ne treba za ispis na konzolu
 import os
-#from sklearn.utils import shuffle #ovo isto? vrv
+#from sklearn.utils import shuffle #ovo isto? vrv TODO
 from sklearn.model_selection import train_test_split
 
-#ovo nam vrv ne treba
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, TensorBoard, ModelCheckpoint
-import io
 
 #---1.7.2021
 from tensorflow.python.framework.ops import disable_eager_execution
-disable_eager_execution()
+#disable_eager_execution()
 #------
 
-#----------------------------
-#Pitanje 1: da li da koristimo sequential
 
 dataPath = "./data"
 labels = ['glioma_tumor', 'no_tumor', 'meningioma_tumor', 'pituitary_tumor']
-
 
 # Setting hyper-parameters
 image_height = 224 # ili 150??
@@ -70,11 +63,10 @@ data_set_labels = np.array(data_set_labels)
 
 # 3264 slike ukupno,
 print(data_set_images.shape)
-# Shuffling (TODO maybe use kaggle shuffle) TODO MOZDA NE RADI
+# Shuffling (TODO  use kaggle shuffle) TODO MOZDA NE RADI
 #np.random.shuffle(data_set_images) izmjeniti u buducnosti
 
-# Image data augmentation here TODO ovde ide i deljenje sa 255
-#datagen_val = ImageDataGenerator(rescale=1./255)
+# Image data augmentation here TODO ovde ide i deljenje sa 255 ILI NE?
 
 #----------------------------------- dodato 1.7.2021
 datagen = ImageDataGenerator(   #dodati i promijeniti neke parametre!
@@ -90,6 +82,7 @@ datagen.fit(data_set_images)
 
 #--------------------------
 print(data_set_images.shape)
+
 # Creating training and testing sets
 train_ratio = 0.70
 validation_ratio = 0.20
@@ -106,7 +99,6 @@ for i in y_train:
 y_train = y_train_new
 y_train = tf.keras.utils.to_categorical(y_train)
 
-
 y_test_new = []
 for i in y_test:
     y_test_new.append(labels.index(i))
@@ -121,6 +113,7 @@ y_val = tf.keras.utils.to_categorical(y_val)
 
 #----
 
+# Transfer learning
 effnet = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(image_height, image_width, 3))
 
 model = effnet.output
@@ -138,10 +131,31 @@ checkpoint = ModelCheckpoint("effnet.h5", monitor="val_accuracy", save_best_only
 reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.3, patience=2, min_delta=0.001,
                               mode='auto', verbose=1)
 
-#history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-#                              epochs=epochs, validation_data=(x_val, y_val),
-#                              steps_per_epoch=x_train.shape[0] // batch_size, callbacks=[tensorboard, checkpoint, reduce_lr])
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                             epochs=epochs, validation_data=(x_val, y_val),
+                             steps_per_epoch=x_train.shape[0] // batch_size,
+                             callbacks=[tensorboard, checkpoint, reduce_lr])
 
-model.fit(x_train, y_train, epochs = epochs, batch_size = batch_size)
+# model.fit(x_train, y_train, epochs = epochs, batch_size = batch_size) #fir sa stack overflow
+
+# history = model.fit(x_train,y_train,validation_split=0.1, epochs =12, verbose=1, batch_size=32)
+#                    #callbacks=[tensorboard,checkpoint,reduce_lr]) #fit sa primarnog lika
 
 
+# Loss graph
+plt.plot(history.history["loss"],c = "purple")
+plt.plot(history.history["val_loss"],c = "orange")
+plt.title("Loss")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend(["train", "test"])
+plt.show()
+
+# Accuracy graph
+plt.plot(history.history["accuracy"],c = "purple")
+plt.plot(history.history["val_accuracy"],c = "orange")
+plt.title("Accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epochs")
+plt.legend(["train", "test"])
+plt.show()
